35ed83a7f96e27ebd4eae3f70821e6a63748305d
Wenhao Zhang <wenhao997@outlook.com>
2022-06-13 00:59:08 +0900

    added demo-Si

---
+++ automaticTB/examples/Si/demo/demo_SALCs.py
@@ -0,0 +1,4 @@
+from automaticTB.run import get_linearcombination
+
+file = "Si_ICSD51688.cif"
+get_linearcombination(file, 3.0)
---
+++ automaticTB/examples/Si/demo/demo_freeparameters.py
@@ -0,0 +1,4 @@
+from automaticTB.run import get_free_parameters
+
+file = "Si_ICSD51688.cif"
+get_free_parameters(file, 3.0)
