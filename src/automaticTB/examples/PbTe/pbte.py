22d27f002eb4139506b65293677cf7fae880a936
Wenhao Zhang <wenhao997@outlook.com>
2022-11-11 11:36:56 +0900

    edit gitignore and remove some tracked file

---
+++ src/automaticTB/examples/PbTe/pbte.py
@@ -0,0 +1,3 @@
+from automaticTB.functions import get_structure_from_cif_file
+
+PbTe = get_structure_from_cif_file("ICSD_CollCode38295.cif", rcut = 4.7)
--- src/automaticTB/examples/__init__.py
+++ src/automaticTB/examples/__init__.py
@@ -1,5 +1,6 @@
-from .AH3.ah3 import *
+from .AH3.AH3 import *
 from .Si.si import *
 from .Perovskite.ao_interaction import *
 from .Perovskite.perovskite import *
+from .PbTe.pbte import *
 from .kpaths import *
